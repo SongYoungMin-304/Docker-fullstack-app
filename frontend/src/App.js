734e5c7c87ee68b9040f1e8e4314d31860c71ff8
sym <sym@humuson.com>
2022-01-09 22:44:09 +0900

    travic ci deploy18

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -49,7 +49,7 @@ function App() {
            <form className="example" onSubmit={submitHandler}>
              <input
               type="text"
-              placeholder="입력해주세요..."
+              placeholder="입력해주세요...(송송송송)"
               onChange={changeHandler}
               value={value}
               />
